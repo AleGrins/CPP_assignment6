00f69dd2c13e7208203822718d87a2b8dcdb2598
AleGrins <37497692+AleGrins@users.noreply.github.com>
2018-05-23 21:58:09 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -8,7 +8,7 @@ const Coordinate Player::play(const Board& board) {}
 
 gameChar Player::getChar() const { return myChar; }
 
-void Player::setChar(const char c)
+void Player::setChar(const char c) const
 {
     try {
 		gameChar tmp (c);
