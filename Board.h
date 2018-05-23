cad28563e3b124f8fbd37d421a6824e01c3d6025
AleGrins <37497692+AleGrins@users.noreply.github.com>
2018-05-23 20:42:47 +0300

    Update Board.h

--- Board.h
+++ Board.h
@@ -35,9 +35,6 @@ class gameChar {
 	public:
 		gameChar();
 		gameChar(const char c_);
-	
-		//assignment from other gameChar
-		const gameChar& operator= (const gameChar& other);
 		
 		bool operator== (const gameChar& other) const;		
 		bool operator== (char c_) const;
