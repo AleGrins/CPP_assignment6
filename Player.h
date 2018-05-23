ff7062fe2f6eca8c7257dc59058605808462389f
AleGrins <37497692+AleGrins@users.noreply.github.com>
2018-05-23 21:58:59 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -9,5 +9,5 @@ public:
 	virtual const string name() const;
 	virtual const Coordinate play(const Board& board);
 	gameChar getChar() const;
-	void setChar(const char c) const;
+	const void setChar(const char c);
 };
