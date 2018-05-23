3c6ed1e73ceef6784d02a25d51c88cde9eae3098
AleGrins <37497692+AleGrins@users.noreply.github.com>
2018-05-23 21:11:40 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -3,9 +3,13 @@
 #include "Board.h"
 
 class Player {
+private:
+    gameChar myChar;
+
 public:
-	gameChar myChar;
 	Player();
 	virtual const string name() const;
 	virtual const Coordinate play(const Board& board);
+	gameChar getChar() const;
+	void setChat(const gameChar c);
 };
