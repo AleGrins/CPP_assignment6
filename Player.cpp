8885addd82a9436ab6d8dba01592e17ba5497acf
AleGrins <37497692+AleGrins@users.noreply.github.com>
2018-05-23 20:58:24 +0300

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -6,14 +6,3 @@ const string Player::name() const {}
 
 const Coordinate Player::play(const Board& board) {}
 
-gameChar Player::getChar() const { return myChar; }
-
-void Player::setChar(const gameChar c) const 
-{
-    try {
-		gameChar tmp (c.getChar());
-    		myChar = tmp;  
-	} catch (const IllegalCharException& ex) {
-		cout << "Illegal char: " << ex.theChar() << endl; 
-	}
-}
